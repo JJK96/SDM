9512937212e7815d2964a47207eac52513c182c6
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2019-11-05 12:20:34 +0100

    Add serialization functions

---
+++ serialization.py
@@ -0,0 +1,13 @@
+def serialize_PKs(PKs):
+    PKs = copy.copy(PKs)
+    for k in ['g', 'X', 'Y']:
+        PKs[k] = PKs['group'].serialize(PKs[k])
+    return PKs
+
+def deserialize_PKs(PKs):
+    for k in ['g', 'X', 'Y']:
+        PKs[k] = PKs['group'].deserialize(PKs[k])
+    return PKs
+
+def serialize_SKg(SKg):
+    SKg = copy.copy(PKs)
